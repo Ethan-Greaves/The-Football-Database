ec1d55819b81ec60401c6f93cafc9b59e2c1191f
Ethan-Greaves <ethangreaves@hotmail.co.uk>
2020-07-20 18:02:12 +0100

    Setup express
    Added server code, index route and express require. App can now be run with local host.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 //#region INITILISATION
+//*Express
+const express               = require('express');
+const app                   = express();
+                           // app.use(express.static("public"));
 //#endregion
 
-
+//#region ROUTES
+app.get(`/`, (req, res) => {
+    res.render(`index.ejs`);
+})
+//#endregion
 //#region SERVER
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`Server has started on port ${port}`));
 //#endregion
