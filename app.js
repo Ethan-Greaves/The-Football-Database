a74df68f5b83f51ce1187914c963e708e64b8d4a
Ethan-Greaves <ethangreaves@hotmail.co.uk>
2020-07-20 21:23:41 +0100

    Created Search page w/ results
    User can search for players which will render the show page and display a list of players with the name that was searched.

--- app.js
+++ app.js
@@ -2,14 +2,51 @@
 //*Express
 const express               = require('express');
 const app                   = express();
-                           app.use(express.static("public"));
+                            app.use(express.static("public"));
+//*Body-Parser
+const bodyParser            = require("body-parser");
+                            app.use(bodyParser.urlencoded({extended: true}));
+
+//*Fetch
+const fetch                 = require('node-fetch');
+
 //#endregion
 
 //#region ROUTES
 app.get(`/`, (req, res) => {
     res.render(`index.ejs`);
 })
+
+app.post(`/search/player`, async (req, res) => {
+    try {
+        //*Get the form data
+        const player = req.body.playerName;
+
+        //*Send request to the api and parse to JSON
+        const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${player}`);
+        const playerData = await response.json();
+        
+        //*Go to the show page and pass through the json data
+        res.render(`show.ejs`, {playerData});
+    } catch (error) {
+        console.error(error);
+    }
+    
+
+    //? Maybe save data to the database
+})
+
+app.post(`/search/team`, (req, res) => {
+    //*Get the form data
+    
+    //*Send request to the api and parse to JSON
+
+    //? Maybe save data to the database (rather than having a global variable be floating around)
+
+
+})
 //#endregion
+
 //#region SERVER
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Server has started on port ${port}`));
