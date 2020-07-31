79170eaeca7047c239fba82b14c3e520026350a1
Ethan-Greaves <ethangreaves@hotmail.co.uk>
2020-07-31 01:19:09 +0100

    Fixed export functions, passed data to team show page
    Fixed the export functions so backend code would be less DRY and now API data is being passed through to the teams show page

--- routes/PlayersRoutes.js
+++ routes/PlayersRoutes.js
@@ -3,51 +3,36 @@
 const express                       = require('express');
 const router                        = express.Router(); 
 //*Module Exports
-const requestDataByName             = require(`../ExportFunctions/requestDataByName.js`);
-const requestDataByID               = require(`../ExportFunctions/requestDataByID.js`);
+const requestDataFromAPI            = require(`../ExportFunctions/requestDataFromAPI.js`);
+
 //*Fetch
 const fetch                         = require('node-fetch');
 //#endregion
 
-//#region PLAYERS
 //TODO This is a global variable, kind of okay because seperation of concern has been used with routes so not really global global, but might be a better way
 let playerData;
 let formData;
 
 router.get(`/`, (req, res) => {
-    //*Retrieve the playerData variable that was stored in the search/player route
-    //const playerData = req.router.get('playerData');
-
     //*Go to the show page and pass through the json data
     res.render(`Players/results.ejs`, {playerData, formData});
 })
 
 router.get(`/:id`, async (req, res) => {
-    //*Retrieve the ID
-    const playerID = req.params.id;
+    try {
+        const playerData = await requestDataFromAPI(`https://www.thesportsdb.com/api/v1/json/1/lookupplayer.php?id=`, req.params.id );
 
-    //*Make a request to the api to retrieve data with the associated ID
-    const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupplayer.php?id=${playerID}`);
-    const playerData = await response.json();
+        //*Render the show page, pass through the data
+        res.render(`Players/show.ejs`, {playerData});
 
-    //*Render the show page, pass through the data
-    res.render(`Players/show.ejs`, {playerData});
-
-    //requestDataByID(res, req.params.id, `https://www.thesportsdb.com/api/v1/json/1/lookupplayer.php?id=`, `Players/show.ejs`);
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 router.post(`/`, async (req, res) => {
     try {
-        //*Get the form data
-        ////const team = requestBodyName;
-
-        //*Send request to the api and parse to JSON
-        const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${req.body.playerName}`);
-        const data = await response.json();
-
-        //*Store the variable in the Express instance
-        //router.set(`playerData`, data);
-        playerData = data;
+        playerData = await requestDataFromAPI(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=`, req.body.playerName);
         formData = req.body;
 
         //*Redirect to another endpoint 
@@ -56,9 +41,6 @@ router.post(`/`, async (req, res) => {
     } catch (error) {
         console.error(error);
     }
-
-    //requestDataByName(router, res, req.body.playerName, `https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=`, `playerData`, `/players`);
 })
-//#endregion
 
 module.exports = router;
--- routes/TeamsRoutes.js
+++ routes/TeamsRoutes.js
@@ -3,42 +3,35 @@
 const express                       = require('express');
 const router                        = express.Router(); 
 //*Module Exports
-const requestDataByName             = require(`../ExportFunctions/requestDataByName.js`);
-const requestDataByID               = require(`../ExportFunctions/requestDataByID.js`);
+const requestDataFromAPI            = require(`../ExportFunctions/requestDataFromAPI.js`);
+
 //*Fetch
 const fetch                         = require('node-fetch');
 //#endregion
 
-//#region TEAMS
 let teamData;
 let formData;
 
 router.get(`/`, (req, res) => {
-    //*Retrieve the playerData variable that was stored in the search/player route
-    //const teamData = req.app.get('teamData');
-
     //*Go to the show page and pass through the json data
     res.render(`Teams/results.ejs`, {teamData, formData});
 })
 
-router.get(`/:id`, (req, res) => {
-    res.send("working");
+router.get(`/:id`, async (req, res) => {
+    try {
+        const teamData = await requestDataFromAPI(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=`, req.params.id);
+
+        //* Render show page and pass throug the team data
+        res.render(`Teams/show.ejs`, {teamData});
+
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 router.post(`/`, async (req, res) => {
-    //requestDataByName(app, res, req.body.teamName, `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=`, `teamData`, `/teams`);
-
     try {
-        //*Get the form data
-        ////const team = requestBodyName;
-
-        //*Send request to the api and parse to JSON
-        const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${req.body.teamName}`);
-        const data = await response.json();
-
-        //*Store the variable in the Express instance
-        //app.set(expressInstanceName, data);
-        teamData = data;
+        teamData = await requestDataFromAPI(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=`, req.body.teamName);
         formData = req.body;
 
         //*Redirect to another endpoint 
@@ -48,7 +41,5 @@ router.post(`/`, async (req, res) => {
         console.error(error);
     }
 })
-
-//#endregion
 
 module.exports = router;
