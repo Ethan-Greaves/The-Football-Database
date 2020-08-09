1294711234c038bcc3c5a9ce2bc80bd545d1d60c
Ethan-Greaves <ethangreaves@hotmail.co.uk>
2020-08-09 16:49:11 +0100

    Added remove feature
    Favourites can now be removed from the database on the index page

--- routes/FavouritesRoutes.js
+++ routes/FavouritesRoutes.js
@@ -5,6 +5,10 @@ const router                = express.Router();
 
 //*Models
 const favouritesModel       = require(`../models/favourites/favourites.js`);  
+
+//*Mehtod-override
+const methodOverride        = require(`method-override`);
+                            router.use(methodOverride(`_method`));
 //#endregion
 
 router.post(`/:id`, async (req, res) => {   
@@ -18,7 +22,18 @@ router.post(`/:id`, async (req, res) => {
     } catch (error) {
         console.error(error);
     }
-    
+})
+
+router.delete(`/:id/delete`, async (req, res) =>{
+    try {
+
+       await favouritesModel.find({ID: req.params.id}).deleteOne().exec();
+
+        //*Go back to the index page
+        res.redirect(`/`);
+    } catch (error) {
+        console.error(error);
+    }
 })
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get(`/`, async (req, res) => {
         const favourites = await favouritesModel.find({});
 
         for (const element of favourites) {
-            //? Not sure if the two fetch calls can be done asynchronously to increase speed
+            //TODO Not sure if the two fetch calls can be done asynchronously to increase speed (is taking about FIVE SECONDS currently to load page with 15 favourites)
             //*Search through the IDs of teams
             fav = await requestDataFromAPI(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=`, element.ID.toString());
             //*If teams returns null
